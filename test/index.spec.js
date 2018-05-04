9896d82b9c40c9707b26fe2d50f8966dcd1864d3
Jack Leh <github@rushsoft.de>
2018-05-04 11:12:30 +0200

    add dist for manual testing

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,5 @@
 import {expect} from 'chai'
-import downloadXlsx from '../src/downloadXlsx'
+import downloadXlsx from '../src/index'
 
 describe('downloadXlsx ' + new Date(), () => {
 	it('is available', () => {
